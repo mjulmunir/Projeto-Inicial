b50609697ac807df6682ffecae7b2bb42c0b0638
Vinícius de Carvalho <vincarvalho99@gmail.com>
2023-09-17 11:01:28 -0300

    Criando a listagem dos itens

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -22,11 +22,10 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
 
     public void cadastrarProduto(ProdutosDTO produto) {
-
         try {
             conn = new conectaDAO().connectDB();
 
-            prep = conn.prepareStatement("INSERT INTO produtos (nome, valor, status) VALUES (?,?,?)");
+            prep = conn.prepareStatement("INSERT INTO produtos (nome, valor, status) VALUES (?,?,?);");
             prep.setString(1, produto.getNome());
             prep.setInt(2, produto.getValor());
             prep.setString(3, produto.getStatus());
@@ -37,10 +36,34 @@ public class ProdutosDAO {
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Erro no acesso ao Bando de Dados!");
         }
-
     }
 
     public ArrayList<ProdutosDTO> listarProdutos() {
+        try {
+            conn = new conectaDAO().connectDB();
+
+            prep = conn.prepareStatement("SELECT * FROM produtos;");
+
+            resultset = prep.executeQuery();
+
+            ProdutosDTO produto;
+
+            do {
+                resultset.next();
+
+                produto = new ProdutosDTO();
+
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+
+                listagem.add(produto);
+                System.out.println(produto.getId());
+            }while (!resultset.isLast());
+        } catch (SQLException ex) {
+            System.out.println("Erro no acesso ao Bando de Dados!");
+        }
 
         return listagem;
     }
